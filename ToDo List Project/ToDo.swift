53d300f62a86fa74e08a520a28763113395c4734
Grace Govan <gigovan35@icloud.com>
2020-05-31 21:07:02 -0400

    In the process of working on iteration 2.

---
+++ ToDo List Project/ToDo.swift
@@ -0,0 +1,18 @@
+//
+//  ToDo.swift
+//  ToDo List Project
+//
+//  Created by Grace Govan on 5/29/20.
+//  Copyright © 2020 Grace Govan. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    
+    var name = ""
+    var important = false
+    
+    
+    
+}
